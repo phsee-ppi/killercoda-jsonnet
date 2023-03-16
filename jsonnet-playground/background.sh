850d1dfe52961f705635d60cf178d3843f5596f0
phsee-ppi <128072351+phsee-ppi@users.noreply.github.com>
2023-03-16 18:11:28 +0100

    download grafana jsonnet vsix extension

--- jsonnet-playground/background.sh
+++ jsonnet-playground/background.sh
@@ -7,4 +7,6 @@ echo starting... # to test stdout output in /var/log/killercoda
 curl -Lo /usr/local/bin/jsonnet https://github.com/CertainLach/jrsonnet/releases/download/v0.4.2/jrsonnet-linux-gnu-amd64
 chmod +x /usr/local/bin/jsonnet
 
+curl -Lo grafana-jsonnet.vsix https://open-vsx.org/api/Grafana/vscode-jsonnet/0.3.3/file/Grafana.vscode-jsonnet-0.3.3.vsix
+
 touch /tmp/finished
